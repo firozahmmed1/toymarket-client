23b12d805f4a2a990a4b649f9639d72048e28325
firozahmmed1 <firozahmmed05@gmail.com>
2023-05-18 23:52:59 +0600

    seven-update

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../../shared/Header/Header";
+import TrendingProduct from "../Trending/TrendingProduct";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <TrendingProduct></TrendingProduct>
         </div>
     );
 };
