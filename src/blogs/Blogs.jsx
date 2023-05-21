fe45bf9c3b39a8c4a38e8845fcba43f0c17203c0
firozahmmed1 <firozahmmed05@gmail.com>
2023-05-21 20:38:16 +0600

    final-update

--- src/blogs/Blogs.jsx
+++ src/blogs/Blogs.jsx
@@ -2,7 +2,18 @@
 const Blogs = () => {
     return (
         <div>
-            <h1>Blog Page</h1>
+            <h1 className="text-xl mb-2 font-semibold">1. What is an access token and refresh token? How do they work and where should we store them on the client-side?</h1>
+            <p>A refresh token allows the user to get a new access token without needing to log in again.</p>
+            <p>Storing and manipulating data in the browser — also known as client-side storage — is useful when it’s not necessary or practical to send it to the web server.</p>
+            <h1 className="text-xl mb-2 mt-2 font-semibold">2.Compare SQL and NoSQL databases?</h1>
+            <p>Explore key differences between SQL and NoSQL databases and learn which type of database is best for various use cases.
+                is a decades-old method for accessing relational databases, and most who work with databases are familiar with it. As unstructured data, amounts of storage and processing power and types of analytics have changed over the years, however, we’ve seen different database technologies become available that are a better fit for newer types of use cases. These databases are commonly called NoSQL.</p>
+            <h1 className="text-xl mb-2 mt-2 font-semibold">3.What is express js? What is Nest JS ?</h1>
+            <p>Express is a node js web application framework that provides broad features for building web and mobile applications. It is used to build a single page, multipage, and hybrid web application.
+                It's a layer built on the top of the Node js that helps manage servers and routes.</p>
+            <p>Nest. js is a server-side Node. js framework that's great for building highly testable and maintainable backend applications. You can create countless types of applications with Node. js; you're only limited by your applications' features.</p>
+            <h1 className="text-xl mb-2 mt-2 font-semibold">4.What is MongoDB aggregate and how does it work?</h1>
+            <p className="mb-12">Aggregation is a way of processing a large number of documents in a collection by means of passing them through different stages. The stages make up what is known as a pipeline. The stages in a pipeline can filter, sort, group, reshape and modify documents that pass through the pipeline.</p>
         </div>
     );
 };
--- src/pages/Home/Trending/TrendingProduct.jsx
+++ src/pages/Home/Trending/TrendingProduct.jsx
@@ -1,22 +1,33 @@
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from "react";
 
 const TrendingProduct = () => {
+    const [data, setData] = useState();
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/allproducts')
+        .then(res=> res.json())
+        .then(data=>setData(data))
+    },[])
     return (
         <div className="mt-20 space-y-4 ">
             <h1 className="text-4xl font-bold text-center italic text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-700">Trending Product</h1>
             <p className="text-center leading-6">The Interactive Learning Toy is a cutting-edge educational device designed to engage and entertain  <br /> children while fostering their cognitive development. This innovative toy combines interactive features, stimulating activities, and  adaptive learning algorithms to create an immersive and personalized learning experience.</p>
             <div className="grid sm:grid-cols-1 md:grid-cols-4 gap-5">
+                {data?.slice(0,4)?.map(mp=> (
             <div className="w-full h-96 rounded relative bg-slate-200 flex items-center justify-center">
                 <div className="avatar">
                     <div className="w-48 rounded">
-                        <img src="https://i.ibb.co/Wz6TDpz/mexican-meatball-subs-41393-1.jpg" />
+                        <img src={mp?.product_url} />
                     </div>
                 </div>
                 <div className="absolute bottom-4 left-5">
-                        <span>Icon</span>
-                        <p className="font-semibold">Name</p>
-                        <p className="font-semibold text-orange-500">$ 190.12</p>
+                        
+                        <p className="font-semibold text-orange-500">$ {mp?.price}</p>
                     </div>
             </div>
+
+                ))}
             </div>
         </div>
     );
